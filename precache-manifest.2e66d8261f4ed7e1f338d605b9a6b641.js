6781f33d83c8780efde37bd1838647775eee457b
Rajat Chaurasia <er.rajat153@gmail.com>
2023-05-10 01:49:32 +0530

    Updates

---
+++ precache-manifest.2e66d8261f4ed7e1f338d605b9a6b641.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "95872a0431b37e5e901a",
+    "url": "/crypto_exchange/static/css/main.cf199fcf.chunk.css"
+  },
+  {
+    "revision": "95872a0431b37e5e901a",
+    "url": "/crypto_exchange/static/js/main.95872a04.chunk.js"
+  },
+  {
+    "revision": "6188222ca2572899ac13",
+    "url": "/crypto_exchange/static/js/1.6188222c.chunk.js"
+  },
+  {
+    "revision": "5dc549cef1e2b4a55fc6",
+    "url": "/crypto_exchange/static/js/runtime~main.5dc549ce.js"
+  },
+  {
+    "revision": "61c51bc3c0c9afbd89bf0e04916daa49",
+    "url": "/crypto_exchange/static/media/ethbtc.61c51bc3.png"
+  },
+  {
+    "revision": "a577c2c6a86259644639693e1bd6556e",
+    "url": "/crypto_exchange/static/media/matic.a577c2c6.png"
+  },
+  {
+    "revision": "103b682c0407c99b07b437aec181495f",
+    "url": "/crypto_exchange/static/media/sol.103b682c.png"
+  },
+  {
+    "revision": "dccb65ee3a2193aa03db1637c8e75ae7",
+    "url": "/crypto_exchange/static/media/qtum.dccb65ee.png"
+  },
+  {
+    "revision": "b4d6edd6bc3abf20ca6d3a092856e532",
+    "url": "/crypto_exchange/static/media/amb.b4d6edd6.png"
+  },
+  {
+    "revision": "0175fc45f1bf7630a5d4ab34c468bb6e",
+    "url": "/crypto_exchange/static/media/aave.0175fc45.png"
+  },
+  {
+    "revision": "38586cf0841b7bedc99791df764aefd7",
+    "url": "/crypto_exchange/index.html"
+  }
+];
